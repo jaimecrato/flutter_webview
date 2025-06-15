a34bf5a2d09d754e148dc460dc12f8be47650c41
Vasco Ranito <Vasco Ranito>
2025-06-16 00:48:39 +0100

    fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.poc.android.mytap"
+    namespace = "com.poc.android.tapcargo"
     compileSdk = flutter.compileSdkVersion
     //ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.poc.android.mytap"
+        applicationId = "com.poc.android.tapcargo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
